663ca6370aa2f30902a2f7b5a9b3f66dd48c811f
pjlehtim <pjlehtim>
2004-02-12 10:31:14 +0000

    Commented

--- core_src/src/org/jArmyTool/data/dataBeans/armylist/ArmylistWargearGroup.java
+++ core_src/src/org/jArmyTool/data/dataBeans/armylist/ArmylistWargearGroup.java
@@ -10,28 +10,50 @@ package org.jArmyTool.data.dataBeans.armylist;
 import java.util.*;
 import java.io.Serializable;
 /**
- *
- * @author  pasi
+ * This class describes one Wargear group
+ * @author Pasi Lehtimäki
  */
 public class ArmylistWargearGroup implements Serializable{
     
     private String name;
     private LinkedList items;
     
-    /** Creates a new instance of ArmylistWargearGroup */
+    private LinkedList subGroups;
+    
+    /**
+     * Creates a new instance of ArmylistWargearGroup
+     * @param name Name of this group.
+     * Please note that this is used as unique identifier.
+     * Two groups with same name can cause some problems.
+     */
     public ArmylistWargearGroup(String name) {
         this.name = name;
         this.items = new LinkedList();
+        this.subGroups = new LinkedList();
     }
     
+    /**
+     * Change name
+     * @param name New name.
+     * Please note that this is used as unique identifier.
+     * Two groups with same name can cause some problems.
+     */    
     public void setName(String name){
         this.name = name;
     }
     
+    /**
+     * Get the name
+     * @return Name of this group
+     */    
     public String getName(){
         return this.name;
     }
     
+    /**
+     * Add new wargear item into this group
+     * @param item The item to be added
+     */    
     public void addItem(ArmylistWargearItem item){
         int index = 0;
         Iterator iterator = this.items.iterator();
@@ -45,14 +67,29 @@ public class ArmylistWargearGroup implements Serializable{
         this.items.add(index, item);
     }
     
+    /**
+     * Remove one item from this group
+     * @param item Item to be removed
+     */    
     public void removeItem(ArmylistWargearItem item){
         this.items.remove(item);
     }
     
+    /**
+     * Get all items in this group
+     * @return Collection of ArmylistWargearItem objects.
+     */    
     public Collection getItems(){
         return Collections.unmodifiableCollection(this.items);
     }
     
+    /**
+     * This methos searches all items of this group until it encounters
+     * one by given name.
+     * If no matching item is found it returns <I>null</I>
+     * @return One item
+     * @param name Name of item
+     */    
     public ArmylistWargearItem getItemByName(String name){
         Iterator iterator = this.items.iterator();
         while(iterator.hasNext()){
